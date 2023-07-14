fd74787ec96adebdf6e2a6646ad532ed7dac13fc
Jairaj <jairaju@mobiquityinc.com>
2023-07-14 11:22:10 +0530

    Day 3

--- Day_3/bookManager/middleware/validate.js
+++ Day_3/bookManager/middleware/validate.js
@@ -1,14 +1,7 @@
 import bookSchema from '../Schema/bookSchema';
 
-const validate = (req,res,next)=>{
-    const {error} = bookSchema.validate(req.body)
-    if(error){
-        const {details} = error
-        const message = details.map(errorDetail => errorDetail.message).join(',')
-        res.json({error:message})
-    }else{
-        next();
-    }
+const validate = (req,res)=>{
+    
 }
 
 export {validate}
